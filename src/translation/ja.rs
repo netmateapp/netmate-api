c8eed2e76616fe2f25634ba7478cff7af811088e
amata1219 <suzukiamata@gmail.com>
2024-08-26 21:07:01 +0900

    追加: メールの送信者名の翻訳

--- src/translation/ja.rs
+++ src/translation/ja.rs
@@ -1,3 +1,15 @@
+pub mod email {
+    pub const SENDER_NAME: &str = "ネットメイト";
+}
+
 pub mod sign_up {
     pub const AUTHENTICATION_EMAIL_SUBJECT: &str = "メールアドレスの認証をしてください。";
+    pub const ATUHENTICATION_EMAIL_BODY_HTML: &str = concat!(
+        "<p>次のリンクをクリックし、メールアドレスを認証を完了してください。</p>",
+        "<p><a href=\"https://netmate.app/verify-email/{token}\">https://netmate.app/verify-email/{token}</a></p>",
+    );
+    pub const AUTHENTICATION_EMAIL_BODY_PLAIN: &str = concat!(
+        "次のリンクをクリックし、メールアドレスを認証を完了してください。",
+        "https://netmate.app/verify-email/{token}",
+    );
 }
--- src/translation/us_en.rs
+++ src/translation/us_en.rs
@@ -1,3 +1,15 @@
+pub mod email {
+    pub const SENDER_NAME: &str = "Netmate";
+}
+
 pub mod sign_up {
     pub const AUTHENTICATION_EMAIL_SUBJECT: &str = "Please verify your email address.";
+    pub const ATUHENTICATION_EMAIL_BODY_HTML: &str = concat!(
+        "<p>Please click the following link to complete the verification of your email address.</p>",
+        "<p><a href=\"https://netmate.app/verify-email/{token}\">https://netmate.app/verify-email/{token}</a></p>"
+    );
+    pub const AUTHENTICATION_EMAIL_BODY_PLAIN: &str = concat!(
+        "Please click the following link to complete the verification of your email address.",
+        "https://netmate.app/verify-email/{token}",
+    );
 }
