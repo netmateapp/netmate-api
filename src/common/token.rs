b971f4535407e2f43db92930236e606e88c21147
amata1219 <suzukiamata@gmail.com>
2024-08-30 14:45:48 +0900

    改善: コードの可読性の向上

--- src/common/token.rs
+++ src/common/token.rs
@@ -5,35 +5,44 @@ use rand_chacha::ChaCha20Rng;
 use serde::{de, Deserialize};
 use thiserror::Error;
 
-const ENTROPY_PER_CHAR: usize = 5;
-const TOKEN_CHAR_SET: [char; 1 << ENTROPY_PER_CHAR] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5'];
+const ENTROPY_BITS_PER_CHAR: usize = 5;
+const TOKEN_CHARSET: [char; 1 << ENTROPY_BITS_PER_CHAR] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5'];
 
-pub struct Token<const BYTES: usize>(String);
+pub struct Token<const ENTROPY_BYTES: usize>(String);
 
-impl<const BYTES: usize> Token<BYTES> {
+const OCTET: usize = 8;
+
+// 特化トークン型の定義に使用可能なヘルパー関数
+pub const fn calc_entropy_bytes(entropy_bits: usize) -> usize {
+    entropy_bits / OCTET
+}
+
+impl<const ENTROPY_BYTES: usize> Token<ENTROPY_BYTES> {
     pub fn gen() -> Self {
         let mut rng = ChaCha20Rng::from_entropy();
-        let mut random_bytes = [0u8; BYTES];
+        let mut random_bytes = [0u8; ENTROPY_BYTES];
         rng.fill_bytes(&mut random_bytes);
 
-        let mut token = String::with_capacity(BYTES * 8 / ENTROPY_PER_CHAR);
+        let mut token = String::with_capacity(ENTROPY_BYTES * OCTET / ENTROPY_BITS_PER_CHAR);
         let mut bit_buffer: u32 = 0;
         let mut bit_count = 0;
 
+        const MASK: u32 = 1 << ENTROPY_BITS_PER_CHAR - 1;
+
         for byte in random_bytes.iter() {
             bit_buffer |= (*byte as u32) << bit_count;
-            bit_count += 8;
+            bit_count += OCTET;
 
-            while bit_count >= 5 {
-                let index = (bit_buffer & 0x1F) as usize;
-                token.push(TOKEN_CHAR_SET[index]);
-                bit_buffer >>= 5;
-                bit_count -= 5;
+            while bit_count >= ENTROPY_BITS_PER_CHAR {
+                let index = (bit_buffer & MASK) as usize;
+                token.push(TOKEN_CHARSET[index]);
+                bit_buffer >>= ENTROPY_BITS_PER_CHAR;
+                bit_count -= ENTROPY_BITS_PER_CHAR;
             }
         }
 
         if bit_count > 0 {
-            token.push(TOKEN_CHAR_SET[(bit_buffer & 0x1F) as usize]);
+            token.push(TOKEN_CHARSET[(bit_buffer & MASK) as usize]);
         }
 
         Self(token)
