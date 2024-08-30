a47f61aa2f3107cbaf1381385d3bbe12b77303e4
amata1219 <suzukiamata@gmail.com>
2024-08-30 15:11:19 +0900

    整理: 定数名の表現を変更

--- src/routes/accounts/creation/sign_up/value.rs
+++ src/routes/accounts/creation/sign_up/value.rs
@@ -1,44 +1,5 @@
-use rand::{RngCore, SeedableRng};
-use rand_chacha::ChaCha20Rng;
+use crate::common::token::{calc_entropy_bytes, Token};
 
-pub struct OneTimeToken(String);
+const TOKEN_ENTROPY_BITS: usize = 120;
 
-impl OneTimeToken {
-    pub fn new_unchecked(token: &str) -> Self {
-        Self(String::from(token))
-    }
-
-    pub fn value(&self) -> &String {
-        &self.0
-    }
-
-    pub fn generate() -> OneTimeToken {
-        let charset: Vec<char> = "abcdefghijklmnopqrstuvwxyz012345".chars().collect();
-
-        let mut rng = ChaCha20Rng::from_entropy();
-        let mut random_bytes = [0u8; 15];
-        rng.fill_bytes(&mut random_bytes);
-
-        let mut token = String::with_capacity(24);
-        let mut bit_buffer: u32 = 0;
-        let mut bit_count = 0;
-
-        for byte in random_bytes.iter() {
-            bit_buffer |= (*byte as u32) << bit_count;
-            bit_count += 8;
-
-            while bit_count >= 5 {
-                let index = (bit_buffer & 0x1F) as usize;
-                token.push(charset[index]);
-                bit_buffer >>= 5;
-                bit_count -= 5;
-            }
-        }
-
-        if bit_count > 0 {
-            token.push(charset[(bit_buffer & 0x1F) as usize]);
-        }
-
-        OneTimeToken(token)
-    }
-}
+pub type OneTimeToken = Token<{calc_entropy_bytes(TOKEN_ENTROPY_BITS)}>;
