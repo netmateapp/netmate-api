135accff432dc9dddc20db3ed54903e13e12f2b7
amata1219 <suzukiamata@gmail.com>
2024-09-03 20:12:25 +0900

    追加: インタプリタ用のエラーマッパー

--- src/helper/error.rs
+++ src/helper/error.rs
@@ -2,6 +2,7 @@ use std::marker::PhantomData;
 
 use thiserror::Error;
 
+// エンドポイントで使用する
 #[derive(Debug, Error)]
 #[error("初期化に失敗しました")]
 pub struct InitError<T>(#[source] anyhow::Error, PhantomData<fn() -> T>);
@@ -10,4 +11,9 @@ impl<T> InitError<T> {
     pub fn new(error: anyhow::Error, _phantom: PhantomData<fn() -> T>) -> Self {
         InitError(error, _phantom)
     }
+}
+
+// インタプリタで使用する
+pub trait DslErrorMapper<T, E> {
+    fn map_dsl_error(self) -> Result<T, E>;
 }
