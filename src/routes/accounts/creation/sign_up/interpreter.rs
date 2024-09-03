489899dd5f327b84217d96c78a8528ef5dce17cf
amata1219 <suzukiamata@gmail.com>
2024-09-03 20:45:32 +0900

    整理: 共通関数への置き換え

--- src/routes/accounts/creation/sign_up/interpreter.rs
+++ src/routes/accounts/creation/sign_up/interpreter.rs
@@ -49,9 +49,9 @@ impl SignUp for SignUpImpl {
     }
 
     async fn apply_to_create_account(&self, email: &Email, pw_hash: &PasswordHash, birth_year: &BirthYear, region: &Region, language: &Language, token: &OneTimeToken) -> Result<(), SignUpError> {
-        let birth_year = birth_year_to_i16(birth_year);
-        let region = region_to_i8(region);
-        let language = language_to_i8(language);
+        let birth_year = i16::from(*birth_year);
+        let region = i8::from(*region);
+        let language = i8::from(*language);
 
         self.session
             .execute(&self.insert_account_creation_application, (token.value(), email.value(), pw_hash.value(), birth_year, region, language))
@@ -85,52 +85,14 @@ impl SignUp for SignUpImpl {
     }
 }
 
-fn birth_year_to_i16(birth_year: &BirthYear) -> i16 {
-    u16::from(*birth_year) as i16
-}
-
-fn region_to_i8(region: &Region) -> i8 {
-    u8::from(*region) as i8
-}
-
-fn language_to_i8(language: &Language) -> i8 {
-    u8::from(*language) as i8
-}
-
 #[cfg(test)]
 mod tests {
     use std::str::FromStr;
 
-    use crate::{common::{birth_year::{BirthYear, MAX_BIRTH_YEAR, MIN_BIRTH_YEAR}, email::Email, language::Language, region::Region, send_email::{NetmateEmail, Subject}}, routes::accounts::creation::sign_up::interpreter::{birth_year_to_i16, language_to_i8, region_to_i8}, translation::{ja, us_en}};
+    use crate::{common::{email::Email, send_email::{NetmateEmail, Subject}}, translation::{ja, us_en}};
 
     use super::{SignUpError, AUTHENTICATION_EMAIL_ADDRESS};
 
-    // `apply_to_create_account`関連のテスト
-    #[test]
-    fn birth_year() {
-        let unspecified = BirthYear::try_from(0).unwrap();
-        assert_eq!(birth_year_to_i16(&unspecified) as u16, 0);
-
-        let min_birth_year = BirthYear::try_from(MIN_BIRTH_YEAR).unwrap();
-        assert_eq!(birth_year_to_i16(&min_birth_year) as u16, MIN_BIRTH_YEAR);
-
-        let max_birth_year = BirthYear::try_from(*MAX_BIRTH_YEAR).unwrap();
-        assert_eq!(birth_year_to_i16(&max_birth_year) as u16, *MAX_BIRTH_YEAR);
-    }
-
-    #[test]
-    fn region() {
-        assert_eq!(region_to_i8(&Region::Afghanistan) as u8, 0);
-        assert_eq!(region_to_i8(&Region::Zimbabwe) as u8, 197);
-    }
-
-    #[test]
-    fn language() {
-        assert_eq!(language_to_i8(&Language::AmericanEnglish) as u8, 0);
-        assert_eq!(language_to_i8(&Language::TaiwaneseMandarin) as u8, 3);
-    }
-
-    // `send_verification_email`関連のテスト
     #[test]
     fn sender_email() {
         let from = match Email::from_str(AUTHENTICATION_EMAIL_ADDRESS) {
