69fd262d957963344a7dde537fec9081eec66878
amata1219 <suzukiamata@gmail.com>
2024-09-07 17:25:30 +0900

    修正: テスト

--- src/middlewares/rate_limit/dsl.rs
+++ src/middlewares/rate_limit/dsl.rs
@@ -7,62 +7,64 @@ use tower::Service;
 use crate::common::{api_key::ApiKey, fallible::Fallible, unixtime::UnixtimeMillis};
 
 pub(crate) trait RateLimit {
-    async fn rate_limit<S, B>(&self, inner: &mut S, request: Request<B>) -> Fallible<S::Response, LimitRateError>
+    async fn rate_limit<S, B>(&self, inner: &mut S, request: Request<B>) -> Fallible<S::Response, RateLimitError>
     where
         S: Service<Request<B>, Error = Infallible, Response = Response<B>>
     {
         match extract_no_account_user_api_key(request.headers()) {
             Some(api_key) => {
-                match self.get_rate(&api_key).await? {
-                    Some(rate) => {
+                match self.check_api_key_exists(&api_key).await? {
+                    Some(last_api_key_refreshed_at) => {
+                        let rate = self.increment_rate(&api_key).await?;
                         if self.is_limit_over(rate) {
-                            Err(LimitRateError::RateLimitOver)
+                            Err(RateLimitError::RateLimitOver)
                         } else {
                             // `Error`は`Infallible`であるため`unwrap()`で問題ない
                             let response = inner.call(request).await.unwrap();
+
+                            if self.should_refresh_api_key(&last_api_key_refreshed_at) {
+                                // エラーが発生しても続行
+                                let _ = self.refresh_api_key(&api_key);
+                            }
+
                             Ok(response)
                         }
                     },
-                    None => {
-                        match self.check_api_key_exists(&api_key).await? {
-                            Some(api_key_refresh_timestamp) => {
-                                if self.should_refresh_api_key(&api_key_refresh_timestamp) {
-                                    // エラーが発生しても続行
-                                    let _ = self.refresh_api_key(&api_key).await;
-                                }
-                                // `Error`は`Infallible`であるため`unwrap()`で問題ない
-                                let response = inner.call(request).await.unwrap();
-                                Ok(response)
-                            },
-                            None => Err(LimitRateError::InvalidApiKey)
-                        }
-                    }
+                    None => Err(RateLimitError::InvalidApiKey),
                 }
             },
-            None => Err(LimitRateError::NoApiKey)
+            None => Err(RateLimitError::NoApiKey)
         }
     }
 
-    async fn get_rate(&self, api_key: &ApiKey) -> Fallible<Option<u16>, LimitRateError>;
+    async fn check_api_key_exists(&self, api_key: &ApiKey) -> Fallible<Option<ApiKeyRefreshTimestamp>, RateLimitError>;
 
-    fn is_limit_over(&self, rate: u16) -> bool;
+    async fn increment_rate(&self, api_key: &ApiKey) -> Fallible<u16, RateLimitError>;
 
-    // 複数のエンドポイントで同じ検証をするのは効率が悪いので、
-    // 30分～1時間程度の短時間キャッシュを行うべき(リフレッシュ時刻も併せてキャッシュするため、短時間にする必要がある)
-    async fn check_api_key_exists(&self, api_key: &ApiKey) -> Fallible<Option<ApiKeyRefreshTimestamp>, LimitRateError>;
+    fn is_limit_over(&self, rate: u16) -> bool {
+        rate > self.limit()
+    }
+
+    fn limit(&self) -> u16;
 
     fn should_refresh_api_key(&self, api_key_refresh_timestamp: &ApiKeyRefreshTimestamp) -> bool;
 
-    async fn refresh_api_key(&self, api_key: &ApiKey) -> Fallible<(), LimitRateError>;
+    async fn refresh_api_key(&self, api_key: &ApiKey) -> Fallible<(), RateLimitError>;
 
 }
 
-#[derive(Debug, Error, PartialEq)]
-pub enum LimitRateError {
+#[derive(Debug, Error)]
+pub enum RateLimitError {
     #[error("APIキーがありません")]
     NoApiKey,
+    #[error("レートの取得に失敗しました")]
+    IncrementRateFailed(#[source] anyhow::Error),
     #[error("レート上限に達しています")]
     RateLimitOver,
+    #[error("APIキーの存在確認に失敗しました")]
+    CheckApiKeyExistsFailed(#[source] anyhow::Error),
+    #[error("APIキーのTTLのリフレッシュに失敗しました")]
+    RefreshApiKeyFailed(#[source] anyhow::Error),
     #[error("無効なAPIキーです")]
     InvalidApiKey,
 }
@@ -95,45 +97,44 @@ mod tests {
 
     use crate::common::{api_key::ApiKey, fallible::Fallible, unixtime::UnixtimeMillis};
 
-    use super::{ApiKeyRefreshTimestamp, LimitRateError, RateLimit};
+    use super::{ApiKeyRefreshTimestamp, RateLimitError, RateLimit};
 
     struct MockRateLimit;
 
     static WITHIN_LIMIT_API_KEY: LazyLock<ApiKey> = LazyLock::new(|| ApiKey::gen());
     static OVER_LIMIT_API_KEY: LazyLock<ApiKey> = LazyLock::new(|| ApiKey::gen());
-    static NO_RATE_API_KEY: LazyLock<ApiKey> = LazyLock::new(|| ApiKey::gen());
     static INVALID_API_KEY: LazyLock<ApiKey> = LazyLock::new(|| ApiKey::gen());
 
     const LIMIT: u16 = 5;
 
     impl RateLimit for MockRateLimit {
-        async fn get_rate(&self, api_key: &ApiKey) -> Fallible<Option<u16>, LimitRateError> {
-            if api_key == &*WITHIN_LIMIT_API_KEY {
-                Ok(Some(LIMIT - 1))
-            } else if api_key == &*OVER_LIMIT_API_KEY {
-                Ok(Some(LIMIT + 1))
-            } else {
-                Ok(None)
-            }
-        }
-
-        fn is_limit_over(&self, rate: u16) -> bool {
-            rate > LIMIT
-        }
-
-        async fn check_api_key_exists(&self, api_key: &ApiKey) -> Fallible<Option<ApiKeyRefreshTimestamp>, LimitRateError> {
-            if api_key == &*NO_RATE_API_KEY {
+        async fn check_api_key_exists(&self, api_key: &ApiKey) -> Fallible<Option<ApiKeyRefreshTimestamp>, RateLimitError> {
+            if api_key == &*WITHIN_LIMIT_API_KEY || api_key == &*OVER_LIMIT_API_KEY {
                 Ok(Some(ApiKeyRefreshTimestamp::new(UnixtimeMillis::now())))
             } else {
                 Ok(None)
             }
         }
 
+        async fn increment_rate(&self, api_key: &ApiKey) -> Fallible<u16, RateLimitError> {
+            if api_key == &*WITHIN_LIMIT_API_KEY {
+                Ok(LIMIT - 1)
+            } else if api_key == &*OVER_LIMIT_API_KEY {
+                Ok(LIMIT + 1)
+            } else {
+                Ok(1)
+            }
+        }
+
+        fn limit(&self) -> u16 {
+            LIMIT
+        }
+
         fn should_refresh_api_key(&self, _api_key_refresh_timestamp: &ApiKeyRefreshTimestamp) -> bool {
             true
         }
 
-        async fn refresh_api_key(&self, _api_key: &ApiKey) -> Fallible<(), LimitRateError> {
+        async fn refresh_api_key(&self, _api_key: &ApiKey) -> Fallible<(), RateLimitError> {
             Ok(())
         }
     }
@@ -154,7 +155,7 @@ mod tests {
         }
     }
 
-    async fn test_rate_limit(api_key: Option<&ApiKey>) -> Fallible<Response<()>, LimitRateError> {
+    async fn test_rate_limit(api_key: Option<&ApiKey>) -> Fallible<Response<()>, RateLimitError> {
         let mut inner = MockInnerService;
 
         let mut request = Request::new(());
@@ -177,24 +178,27 @@ mod tests {
     #[tokio::test]
     async fn test_rate_limit_over_limit() {
         let response = test_rate_limit(Some(&*OVER_LIMIT_API_KEY)).await;
-        assert_eq!(response.unwrap_err(), LimitRateError::RateLimitOver);
-    }
-
-    #[tokio::test]
-    async fn test_rate_limit_no_rate() {
-        let response = test_rate_limit(Some(&*NO_RATE_API_KEY)).await;
-        assert!(response.is_ok());
+        match response.unwrap_err() {
+            RateLimitError::RateLimitOver => (),
+            e => panic!("Unexpected error: {:?}", e),
+        }
     }
 
     #[tokio::test]
     async fn test_rate_limit_invalid_api_key() {
         let response = test_rate_limit(Some(&*INVALID_API_KEY)).await;
-        assert_eq!(response.unwrap_err(), LimitRateError::InvalidApiKey);
+        match response.unwrap_err() {
+            RateLimitError::InvalidApiKey => (),
+            e => panic!("Unexpected error: {:?}", e),
+        }
     }
 
     #[tokio::test]
     async fn test_rate_limit_no_api_key() {
         let response = test_rate_limit(None).await;
-        assert_eq!(response.unwrap_err(), LimitRateError::NoApiKey);
+        match response.unwrap_err() {
+            RateLimitError::NoApiKey => (),
+            e => panic!("Unexpected error: {:?}", e),
+        }
     }
 }
