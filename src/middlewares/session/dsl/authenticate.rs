3a0dec5c84aad4dbcaaff21590b6272f8fe539fd
amata1219 <suzukiamata@gmail.com>
2024-09-08 14:13:45 +0900

    追加: 認証処理

--- src/middlewares/session/dsl/authenticate.rs
+++ src/middlewares/session/dsl/authenticate.rs
@@ -1,37 +1,18 @@
-use std::convert::Infallible;
-
-use http::{Request, Response};
+use http::Request;
 use thiserror::Error;
-use tower::Service;
 
-use crate::common::{id::AccountId, session::value::SessionId};
-
-use super::{dsl::{can_set_cookie_in_response_header, insert_account_id_into_request_extensions}, set_cookie::set_new_session_id_into_response_header};
+use crate::common::{fallible::Fallible, id::AccountId, session::value::SessionId};
 
 pub(crate) trait AuthenticateUser {
-    async fn authenticate<S, B>(&self, inner: &mut S, mut req: Request<B>, session_id: &SessionId, account_id: AccountId) -> S::Response
-    where
-        S: Service<Request<B>, Error = Infallible, Response = Response<B>>,
-    {
-        insert_account_id_into_request_extensions(&mut req, account_id);
-
-        // `Error`は`Infallible`で起こり得ないので`unwrap()`で問題ない
-        let mut response = inner.call(req).await.unwrap();
-
-        // パスワード変更やログアウトによるSet-Cookieヘッダが無い場合のみセッションを延長
-        if can_set_cookie_in_response_header(&mut response) {
-            Self::reset_session_timeout(&mut response, &session_id);
-        }
-
-        response
+    async fn authenticate_user<B>(&self, request: &mut Request<B>, session_id: &SessionId) -> Fallible<(), AuthenticateUserError> {
+        self.resolve_session_id_to_account_id(&session_id)
+            .await?
+            .map(|account_id| request.extensions_mut().insert(account_id))
+            .map(|_| ())
+            .ok_or_else(|| AuthenticateUserError::InvalidSessionId)
     }
 
-
-    fn reset_session_timeout<B>(response: &mut Response<B>, session_id: &SessionId) {
-        // 書き直し必要
-        // 期限の延長はクッキーの再設定により行われるため、実態はセッション識別子の再設定関数である
-        set_new_session_id_into_response_header(response.headers_mut(), session_id);
-    }
+    async fn resolve_session_id_to_account_id(&self, session_id: &SessionId) -> Fallible<Option<AccountId>, AuthenticateUserError>;
 }
 
 #[derive(Debug, Error)]
@@ -40,4 +21,45 @@ pub enum AuthenticateUserError {
     ResolveSessionIdFailed,
     #[error("無効なセッションIDです")]
     InvalidSessionId,
+}
+
+#[cfg(test)]
+mod tests {
+    use std::sync::LazyLock;
+
+    use http::Request;
+
+    use crate::common::{fallible::Fallible, id::{uuid7::Uuid7, AccountId}, session::value::SessionId};
+
+    use super::{AuthenticateUser, AuthenticateUserError};
+
+    static VALID_SESSION_ID: LazyLock<SessionId> = LazyLock::new(|| SessionId::gen());
+
+    struct MockAuthenticateUser;
+
+    impl AuthenticateUser for MockAuthenticateUser {
+        async fn resolve_session_id_to_account_id(&self, session_id: &SessionId) -> Fallible<Option<AccountId>, AuthenticateUserError> {
+            if session_id == &*VALID_SESSION_ID {
+                Ok(Some(AccountId::new(Uuid7::now())))
+            } else {
+                Err(AuthenticateUserError::ResolveSessionIdFailed)
+            }
+        }
+    }
+
+    #[tokio::test]
+    async fn valid_session_id() {
+        let mut request = Request::new(());
+        let result = MockAuthenticateUser.authenticate_user(&mut request, &*VALID_SESSION_ID).await;
+        let is_resolved = request.extensions().get::<AccountId>().is_some();
+        assert!(result.is_ok() && is_resolved);
+    }
+
+    #[tokio::test]
+    async fn invalid_session_id() {
+        let mut request = Request::new(());
+        let result = MockAuthenticateUser.authenticate_user(&mut request, &SessionId::gen()).await;
+        let is_resolved = request.extensions().get::<AccountId>().is_some();
+        assert!(result.is_err() && !is_resolved);
+    }
 }
