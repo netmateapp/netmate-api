80c22eb764989399d08e7c3b5ddff63dfc7b4f7d
amata1219 <suzukiamata@gmail.com>
2024-09-09 00:11:42 +0900

    整理: 関数の名前の調整

--- src/middlewares/session/dsl/update_session.rs
+++ src/middlewares/session/dsl/update_session.rs
@@ -1,4 +1,23 @@
+use thiserror::Error;
+
 use crate::common::{fallible::Fallible, id::AccountId, session::value::SessionId};
+
+pub(crate) trait UpdateSession {
+    async fn update_session(&self, session_account_id: &AccountId, new_expiration: &SessionExpirationTime) -> Fallible<SessionId, UpdateSessionError> {
+        let mut new_session_id = SessionId::gen();
+        
+        // このループは奇跡が起きない限りO(1)となる
+        loop {
+            match self.try_assign_new_session_id_with_expiration_to_account(&new_session_id, session_account_id, new_expiration).await {
+                Ok(()) => return Ok(new_session_id),
+                Err(UpdateSessionError::SessionIdAlreadyUsed) => new_session_id = SessionId::gen(),
+                _ => return Err(UpdateSessionError::AssignNewSessionIdFailed)
+            }
+        }
+    }
+
+    async fn try_assign_new_session_id_with_expiration_to_account(&self, new_session_id: &SessionId, session_account_id: &AccountId, new_expiration: &SessionExpirationTime) -> Fallible<(), UpdateSessionError>;
+}
 
 pub struct SessionExpirationTime(u32);
 
@@ -12,24 +31,10 @@ impl SessionExpirationTime {
     }
 }
 
-pub(crate) trait UpdateSession {
-    async fn update_session(&self, session_account_id: &AccountId, new_expiration: &SessionExpirationTime) -> Fallible<SessionId, UpdateSessionError> {
-        let mut new_session_id = SessionId::gen();
-        
-        // このループは奇跡が起きない限りO(1)となる
-        loop {
-            match self.try_activate_new_session_id_with_expiration(&new_session_id, session_account_id, new_expiration).await {
-                Ok(()) => return Ok(new_session_id),
-                Err(UpdateSessionError::SessionIdAlreadyUsed) => new_session_id = SessionId::gen(),
-                _ => return Err(UpdateSessionError::IssueNewSessionIdFailed)
-            }
-        }
-    }
-
-    async fn try_activate_new_session_id_with_expiration(&self, new_session_id: &SessionId, session_account_id: &AccountId, new_expiration: &SessionExpirationTime) -> Fallible<(), UpdateSessionError>;
-}
-
+#[derive(Debug, Error)]
 pub enum UpdateSessionError {
+    #[error("セッションIDが既に使用されています")]
     SessionIdAlreadyUsed,
-    IssueNewSessionIdFailed,
+    #[error("新規セッションIDの割り当てに失敗しました")]
+    AssignNewSessionIdFailed,
 }
