4d67ef4d16f2a5e3d3f085b46bdbeee20da5de66
amata1219 <suzukiamata@gmail.com>
2024-09-08 21:48:13 +0900

    整理: トレイト名の調整

--- src/middlewares/session/dsl/authenticate.rs
+++ src/middlewares/session/dsl/authenticate.rs
@@ -2,8 +2,8 @@ use thiserror::Error;
 
 use crate::common::{fallible::Fallible, id::AccountId, session::value::SessionId};
 
-pub(crate) trait AuthenticateUser {
-    async fn authenticate(&self, session_id: &SessionId) -> Fallible<AccountId, AuthenticateUserError> {
+pub(crate) trait AuthenticateSession {
+    async fn authenticate_session(&self, session_id: &SessionId) -> Fallible<AccountId, AuthenticateUserError> {
         self.resolve_session_id_to_account_id(&session_id)
             .await?
             .ok_or_else(|| AuthenticateUserError::InvalidSessionId)
@@ -26,13 +26,13 @@ mod tests {
 
     use crate::common::{fallible::Fallible, id::{uuid7::Uuid7, AccountId}, session::value::SessionId};
 
-    use super::{AuthenticateUser, AuthenticateUserError};
+    use super::{AuthenticateSession, AuthenticateUserError};
 
     static VALID_SESSION_ID: LazyLock<SessionId> = LazyLock::new(|| SessionId::gen());
 
     struct MockAuthenticateUser;
 
-    impl AuthenticateUser for MockAuthenticateUser {
+    impl AuthenticateSession for MockAuthenticateUser {
         async fn resolve_session_id_to_account_id(&self, session_id: &SessionId) -> Fallible<Option<AccountId>, AuthenticateUserError> {
             if session_id == &*VALID_SESSION_ID {
                 Ok(Some(AccountId::new(Uuid7::now())))
@@ -44,13 +44,13 @@ mod tests {
 
     #[tokio::test]
     async fn valid_session_id() {
-        let result = MockAuthenticateUser.authenticate(&*VALID_SESSION_ID).await;
+        let result = MockAuthenticateUser.authenticate_session(&*VALID_SESSION_ID).await;
         assert!(result.is_ok());
     }
 
     #[tokio::test]
     async fn invalid_session_id() {
-        let result = MockAuthenticateUser.authenticate(&SessionId::gen()).await;
+        let result = MockAuthenticateUser.authenticate_session(&SessionId::gen()).await;
         assert!(result.is_err());
     }
 }
