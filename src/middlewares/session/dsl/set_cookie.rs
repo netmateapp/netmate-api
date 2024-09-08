4adb2e9c475da3e50f7aa5fe45f17342ad52bdc9
amata1219 <suzukiamata@gmail.com>
2024-09-09 02:49:29 +0900

    改善: クッキー設定関連処理をDSL化

--- src/middlewares/session/dsl/set_cookie.rs
+++ src/middlewares/session/dsl/set_cookie.rs
@@ -1,5 +1,4 @@
-use cookie::Cookie;
-use http::{header::SET_COOKIE, HeaderMap, HeaderName, HeaderValue, Response};
+use http::{header::SET_COOKIE, HeaderName, HeaderValue, Response};
 use time::Duration;
 
 use crate::common::session::value::{secure_cookie_builder, to_cookie_value, SessionSeries, RefreshToken, SessionId, REFRESH_PAIR_COOKIE_KEY, LOGIN_ID_EXPIRY_DAYS, SESSION_COOKIE_KEY, SESSION_TIMEOUT_MINUTES};
@@ -17,116 +16,77 @@ pub(crate) trait SetSessionCookie {
         Self::set_cookie(response, &REFRESH_PAIR_COOKIE_KEY, to_cookie_value(session_series, refresh_token), LOGIN_ID_EXPIRY_DAYS)
     }
 
+    fn clear_session_related_cookie_headers(&self) -> [(HeaderName, HeaderValue); 2] {
+        [
+            (SET_COOKIE, Self::create_cookie_value(&SESSION_COOKIE_KEY, String::from(""), Duration::seconds(0))),
+            (SET_COOKIE, Self::create_cookie_value(&REFRESH_PAIR_COOKIE_KEY, String::from(""), Duration::seconds(0)))
+        ]
+    }
+
     fn set_cookie<B>(response: &mut Response<B>, key: &'static str, value: String, max_age: Duration) {
+        response.headers_mut().insert(SET_COOKIE, Self::create_cookie_value(key, value, max_age));
+    }
+
+    fn create_cookie_value(key: &'static str, value: String, max_age: Duration) -> HeaderValue {
         let cookie = secure_cookie_builder(key, value)
             .max_age(max_age)
             .build();
 
         // Cookieのキー及び値に無効な文字を使用していないため、`unwrap()`で問題ない
-        let header_value = HeaderValue::from_str(cookie.to_string().as_str()).unwrap();
-
-        response.headers_mut().insert(SET_COOKIE, header_value);
-    }
-
-    fn clear_session_cookies(&self) -> [(HeaderName, HeaderValue); 2];
-}
-
-
-// 期限の延長はクッキーの再設定により行われるため、実態はセッション識別子の再設定関数である
-pub fn reset_session_timeout(response_headers: &mut HeaderMap, session_management_id: &SessionId) {
-    set_new_session_id_into_response_header(response_headers, session_management_id);
-}
-
-pub fn set_new_session_id_into_response_header(response_headers: &mut HeaderMap, new_session_management_id: &SessionId) {
-    let cookie = secure_cookie_builder(&SESSION_COOKIE_KEY, new_session_management_id.value().value().clone())
-        .max_age(SESSION_TIMEOUT_MINUTES)
-        .build();
-
-    set_cookie(response_headers, &cookie);
-}
-
-pub fn set_new_refresh_token_in_header(response_headers: &mut HeaderMap, series_id: &SessionSeries, refresh_token: &RefreshToken) {
-    let cookie = secure_cookie_builder(&REFRESH_PAIR_COOKIE_KEY, to_cookie_value(series_id, refresh_token))
-        .max_age(LOGIN_ID_EXPIRY_DAYS)
-        .build();
-
-    set_cookie(response_headers, &cookie);
-}
-
-fn set_cookie(headers: &mut HeaderMap, cookie: &Cookie<'static>) {
-    headers.insert(
-        SET_COOKIE,
         HeaderValue::from_str(cookie.to_string().as_str()).unwrap()
-    );
+    }
 }
-
-pub fn clear_session_ids_in_response_header() -> [(HeaderName, HeaderValue); 2] {
-    let session_management_cookie = secure_cookie_builder(&SESSION_COOKIE_KEY, String::from(""))
-        .max_age(Duration::seconds(0))
-        .build();
-
-    let login_cookie = secure_cookie_builder(&REFRESH_PAIR_COOKIE_KEY, String::from(""));
-
-    [
-        (SET_COOKIE, HeaderValue::from_str(session_management_cookie.to_string().as_str()).unwrap()),
-        (SET_COOKIE, HeaderValue::from_str(login_cookie.to_string().as_str()).unwrap())
-    ]
-}
-
 
 #[cfg(test)]
 mod tests {
     use cookie::Cookie;
-    use http::{header::SET_COOKIE, HeaderMap};
+    use http::{header::SET_COOKIE, Response};
+    use time::Duration;
 
-    use crate::{common::session::value::{to_cookie_value, SessionSeries, RefreshToken, SessionId, REFRESH_PAIR_COOKIE_KEY, LOGIN_ID_EXPIRY_DAYS, SESSION_COOKIE_KEY, SESSION_TIMEOUT_MINUTES}, middlewares::session::dsl::set_cookie::{set_new_refresh_token_in_header, set_new_session_id_into_response_header}};
+    use crate::common::session::value::{to_cookie_value, SessionSeries, RefreshToken, SessionId, REFRESH_PAIR_COOKIE_KEY, SESSION_COOKIE_KEY, SESSION_TIMEOUT_MINUTES};
 
-    use super::set_cookie;
+    use super::SetSessionCookie;
 
-    #[test]
-    fn test_set_new_session_management_id_in_header() {
-        let mut headers = HeaderMap::new();
-        let id = SessionId::gen();
+    struct MockSetSessionCookie;
 
-        set_new_session_id_into_response_header(&mut headers, &id);
+    impl SetSessionCookie for MockSetSessionCookie {}
 
-        let cookie = parse_cookie(&headers);
+    fn test_set_cookie(response: Response<()>, key: &'static str, value: String, max_age: Duration) {
+        let set_cookie_value = response.headers()
+            .get(SET_COOKIE)
+            .unwrap()
+            .to_str()
+            .unwrap();
 
-        assert_eq!(cookie.name(), SESSION_COOKIE_KEY);
-        assert_eq!(cookie.value(), id.value().value().as_str());
-        assert_eq!(cookie.max_age().unwrap(), SESSION_TIMEOUT_MINUTES);
+        let cookie = Cookie::parse(set_cookie_value)
+            .unwrap();
+
+        assert_eq!(cookie.name(), key);
+        assert_eq!(cookie.value(), value);
+        assert_eq!(cookie.max_age().unwrap(), max_age);
     }
 
     #[test]
-    fn test_set_new_login_token_in_header() {
-        let mut headers = HeaderMap::new();
-        let series_id = SessionSeries::gen();
-        let token = RefreshToken::gen();
-
-        set_new_refresh_token_in_header(&mut headers, &series_id, &token);
-
-        let cookie = parse_cookie(&headers);
-
-        assert_eq!(cookie.name(), REFRESH_PAIR_COOKIE_KEY);
-        assert_eq!(cookie.value(), to_cookie_value(&series_id, &token));
-        assert_eq!(cookie.max_age().unwrap(), LOGIN_ID_EXPIRY_DAYS);
+    fn set_session_cookie() {
+        let mut response = Response::new(());
+        let session_id = SessionId::gen();
+        MockSetSessionCookie::set_session_cookie_with_expiry(&mut response, &session_id);
+        test_set_cookie(response, &SESSION_COOKIE_KEY, session_id.to_string(), SESSION_TIMEOUT_MINUTES);
     }
 
     #[test]
-    fn test_set_cookie() {
-        let mut headers = HeaderMap::new();
-        let cookie = Cookie::new("key", "value");
-
-        set_cookie(&mut headers, &cookie);
-
-        let cookie = parse_cookie(&headers);
-
-        assert_eq!(cookie.name(), "key");
-        assert_eq!(cookie.value(), "value");
+    fn set_refresh_pair() {
+        let mut response = Response::new(());
+        let session_series = SessionSeries::gen();
+        let refresh_token = RefreshToken::gen();
+        MockSetSessionCookie::set_refresh_pair_cookie_with_expiry(&mut response, &session_series, &refresh_token);
+        test_set_cookie(response, &REFRESH_PAIR_COOKIE_KEY, to_cookie_value(&session_series, &refresh_token), SESSION_TIMEOUT_MINUTES);
     }
 
-    fn parse_cookie<'a>(headers: &'a HeaderMap) -> Cookie<'a> {
-        let cookie_str = headers.get(SET_COOKIE).unwrap().to_str().unwrap();
-        Cookie::parse(cookie_str).unwrap()
+    #[test]
+    fn set_cookie() {
+        let mut response = Response::new(());
+        MockSetSessionCookie::set_cookie(&mut response, "key", String::from("value"), Duration::seconds(10));
+        test_set_cookie(response, "key", String::from("value"), Duration::seconds(10));
     }
 }
