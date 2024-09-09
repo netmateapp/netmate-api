4c0544b1f52f2fd4e17928cec24939fa26c5b0a4
amata1219 <suzukiamata@gmail.com>
2024-09-09 14:40:05 +0900

    整理: エラー名の変更

--- src/middlewares/session/dsl/authenticate.rs
+++ src/middlewares/session/dsl/authenticate.rs
@@ -3,17 +3,17 @@ use thiserror::Error;
 use crate::common::{fallible::Fallible, id::AccountId, session::value::SessionId};
 
 pub(crate) trait AuthenticateSession {
-    async fn authenticate_session(&self, session_id: &SessionId) -> Fallible<AccountId, AuthenticateUserError> {
+    async fn authenticate_session(&self, session_id: &SessionId) -> Fallible<AccountId, AuthenticateSessionError> {
         self.resolve_session_id_to_account_id(&session_id)
             .await?
-            .ok_or_else(|| AuthenticateUserError::InvalidSessionId)
+            .ok_or_else(|| AuthenticateSessionError::InvalidSessionId)
     }
 
-    async fn resolve_session_id_to_account_id(&self, session_id: &SessionId) -> Fallible<Option<AccountId>, AuthenticateUserError>;
+    async fn resolve_session_id_to_account_id(&self, session_id: &SessionId) -> Fallible<Option<AccountId>, AuthenticateSessionError>;
 }
 
 #[derive(Debug, Error)]
-pub enum AuthenticateUserError {
+pub enum AuthenticateSessionError {
     #[error("セッションIDの解決に失敗しました")]
     ResolveSessionIdFailed,
     #[error("無効なセッションIDです")]
@@ -26,18 +26,18 @@ mod tests {
 
     use crate::common::{fallible::Fallible, id::{uuid7::Uuid7, AccountId}, session::value::SessionId};
 
-    use super::{AuthenticateSession, AuthenticateUserError};
+    use super::{AuthenticateSession, AuthenticateSessionError};
 
     static VALID_SESSION_ID: LazyLock<SessionId> = LazyLock::new(|| SessionId::gen());
 
     struct MockAuthenticateUser;
 
     impl AuthenticateSession for MockAuthenticateUser {
-        async fn resolve_session_id_to_account_id(&self, session_id: &SessionId) -> Fallible<Option<AccountId>, AuthenticateUserError> {
+        async fn resolve_session_id_to_account_id(&self, session_id: &SessionId) -> Fallible<Option<AccountId>, AuthenticateSessionError> {
             if session_id == &*VALID_SESSION_ID {
                 Ok(Some(AccountId::new(Uuid7::now())))
             } else {
-                Err(AuthenticateUserError::ResolveSessionIdFailed)
+                Err(AuthenticateSessionError::ResolveSessionIdFailed)
             }
         }
     }
