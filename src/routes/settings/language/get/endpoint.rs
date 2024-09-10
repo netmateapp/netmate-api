2250bb04166b4cac969cb129af9bee50c276f9b6
amata1219 <suzukiamata@gmail.com>
2024-09-10 14:47:03 +0900

    修正: 引数を一般的なパターンに変更

--- src/routes/settings/language/get/endpoint.rs
+++ src/routes/settings/language/get/endpoint.rs
@@ -1,11 +1,11 @@
 use std::sync::Arc;
 
-use axum::{extract::{Request, State}, routing::get, Json, Router};
+use axum::{extract::State, routing::get, Extension, Json, Router};
 use axum_macros::debug_handler;
 use http::StatusCode;
 use scylla::Session;
 use tower::ServiceBuilder;
-use tracing::{error, info};
+use tracing::info;
 
 use crate::{common::{id::AccountId, language::Language}, helper::{error::InitError, valkey::Pool}, middlewares::session::middleware::LoginSessionLayer, routes::settings::language::get::dsl::GetLanguage};
 
@@ -33,25 +33,15 @@ pub async fn endpoint(db: Arc<Session>, cache: Arc<Pool>) -> Result<Router, Init
 #[debug_handler]
 pub async fn handler(
     State(routine): State<Arc<GetLanguageImpl>>,
-    req: Request,
+    Extension(account_id): Extension<AccountId>,
 ) -> Result<Json<Language>, StatusCode> {
-    // `Extensions`を使うと値が複製されるため、`Request`から直接取得する
-    let account_id = req.extensions()
-        .get::<AccountId>();
-
-    match account_id {
-        Some(account_id) => match routine.get_language(account_id).await {
-            Ok(language) => Ok(Json(language)),
-            Err(e) => {
-                info!(
-                    error = %e,
-                    "アカウントの言語設定を取得できませんでした。"
-                );
-                Err(StatusCode::INTERNAL_SERVER_ERROR)
-            }
-        },
-        None => { // ここに到達したら、認証ミドルウェアに不具合がある
-            error!("認証ミドルウェアに不具合があります。アカウント識別子が提供されなかったため、言語設定を取得できませんでした。");
+    match routine.get_language(&account_id).await {
+        Ok(language) => Ok(Json(language)),
+        Err(e) => {
+            info!(
+                error = %e,
+                "アカウントの言語設定を取得できませんでした。"
+            );
             Err(StatusCode::INTERNAL_SERVER_ERROR)
         }
     }
