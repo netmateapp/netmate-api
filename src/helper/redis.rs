cfc8a09f89b10212d025e436726b9b97bf82499a
amata1219 <suzukiamata@gmail.com>
2024-09-14 15:17:54 +0900

    修正: 非const fnコンストラクタを復活

--- src/helper/redis.rs
+++ src/helper/redis.rs
@@ -2,6 +2,7 @@ use std::fmt::{self, Display};
 
 use bb8_redis::{bb8::{self, PooledConnection, RunError}, RedisConnectionManager};
 use redis::{FromRedisValue, RedisError, ToRedisArgs};
+use thiserror::Error;
 
 pub type Pool = bb8::Pool<RedisConnectionManager>;
 pub type Connection<'a> = PooledConnection<'a, RedisConnectionManager>;
@@ -35,9 +36,35 @@ impl Namespace {
         Namespace(namespace)
     }
 
+    pub fn new(namespace: &'static str) -> Result<Self, ParseNamespaceError> {
+        if namespace.contains(':') {
+            Err(ParseNamespaceError::ContainsColon)
+        } else if !namespace.is_ascii() {
+            Err(ParseNamespaceError::NotAscii)
+        } else if namespace.len() < MIN_NAMESPACE_LENGTH {
+            Err(ParseNamespaceError::TooShort)
+        } else if namespace.len() > MAX_NAMESPACE_LENGTH {
+            Err(ParseNamespaceError::TooLong)
+        } else {
+            Ok(Self(namespace))
+        }
+    }
+
     pub fn value(&self) -> &'static str {
         self.0
     }
+}
+
+#[derive(Debug, Error)]
+pub enum ParseNamespaceError {
+    #[error("コロンは許可されていません")]
+    ContainsColon,
+    #[error("ASCII文字列である必要があります")]
+    NotAscii,
+    #[error("{}文字以上である必要があります", MIN_NAMESPACE_LENGTH)]
+    TooShort,
+    #[error("{}文字以下である必要があります", MAX_NAMESPACE_LENGTH)]
+    TooLong
 }
 
 impl Display for Namespace {
