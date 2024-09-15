779c92d819e27d4f28a93e3d3dc5d20cb1d90b67
amata1219 <suzukiamata@gmail.com>
2024-09-15 15:18:30 +0900

    追加: リフレッシュペア割り当てを表現するDSL

--- src/middlewares/start_session/dsl/assign_refresh_pair.rs
+++ src/middlewares/start_session/dsl/assign_refresh_pair.rs
@@ -1,3 +1,29 @@
-pub(crate) trait AssignRefreshPair {
-    
+use thiserror::Error;
+
+use crate::common::{fallible::Fallible, id::AccountId, session::{refresh_pair_expiration::RefreshPairExpirationSeconds, refresh_token::RefreshToken, session_expiration::SessionExpirationSeconds, session_id::SessionId, session_series::SessionSeries}};
+
+pub(crate)  trait AssignRefreshPair {
+    async fn assign_refresh_pair(&self, session_account_id: &AccountId, expiration: &RefreshPairExpirationSeconds) -> Fallible<(SessionSeries, RefreshToken), AssignRefreshPairError> {
+        let mut session_series = SessionSeries::gen();
+        let refresh_token = RefreshToken::gen();
+
+        // このループは奇跡が起きない限りO(1)となる
+        loop {
+            match self.try_assign_refresh_pair_with_expiration_if_unused(&session_series, &refresh_token, session_account_id, expiration).await {
+                Ok(()) => return Ok((session_series, refresh_token)),
+                Err(AssignRefreshPairError::SessionIdAlreadyUsed) => session_series = SessionSeries::gen(),
+                Err(e) => return Err(e),
+            }
+        }
+    }
+
+    async fn try_assign_refresh_pair_with_expiration_if_unused(&self, session_series: &SessionSeries, refresh_token: &RefreshToken, session_account_id: &AccountId, expiration: &RefreshPairExpirationSeconds) -> Fallible<(), AssignRefreshPairError>;
+}
+
+#[derive(Debug, Error)]
+pub enum AssignRefreshPairError {
+    #[error("セッションIDが既に使用されています")]
+    SessionIdAlreadyUsed,
+    #[error("新規セッションIDの割り当てに失敗しました")]
+    AssignNewSessionIdFailed(#[source] anyhow::Error),
 }
--- src/middlewares/start_session/dsl/assign_session_id.rs
+++ src/middlewares/start_session/dsl/assign_session_id.rs
@@ -3,20 +3,20 @@ use thiserror::Error;
 use crate::common::{fallible::Fallible, id::AccountId, session::{session_expiration::SessionExpirationSeconds, session_id::SessionId}};
 
 pub(crate)  trait AssignSessionId {
-    async fn assign_session_id(&self, session_account_id: &AccountId, new_expiration: &SessionExpirationSeconds) -> Fallible<SessionId, AssignSessionIdError> {
-        let mut new_session_id = SessionId::gen();
+    async fn assign_session_id(&self, session_account_id: &AccountId, expiration: &SessionExpirationSeconds) -> Fallible<SessionId, AssignSessionIdError> {
+        let mut session_id = SessionId::gen();
         
         // このループは奇跡が起きない限りO(1)となる
         loop {
-            match self.try_assign_new_session_id_with_expiration_if_unused(&new_session_id, session_account_id, new_expiration).await {
-                Ok(()) => return Ok(new_session_id),
-                Err(AssignSessionIdError::SessionIdAlreadyUsed) => new_session_id = SessionId::gen(),
+            match self.try_assign_new_session_id_with_expiration_if_unused(&session_id, session_account_id, expiration).await {
+                Ok(()) => return Ok(session_id),
+                Err(AssignSessionIdError::SessionIdAlreadyUsed) => session_id = SessionId::gen(),
                 Err(e) => return Err(e),
             }
         }
     }
 
-    async fn try_assign_new_session_id_with_expiration_if_unused(&self, new_session_id: &SessionId, session_account_id: &AccountId, new_expiration: &SessionExpirationSeconds) -> Fallible<(), AssignSessionIdError>;
+    async fn try_assign_new_session_id_with_expiration_if_unused(&self, session_id: &SessionId, session_account_id: &AccountId, expiration: &SessionExpirationSeconds) -> Fallible<(), AssignSessionIdError>;
 }
 
 #[derive(Debug, Error)]
