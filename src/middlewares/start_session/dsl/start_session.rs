b4b9b2b661a687fa645b0ec348e0a3f5a9f77e79
amata1219 <suzukiamata@gmail.com>
2024-09-15 21:32:52 +0900

    追加: 自動テスト

--- src/middlewares/start_session/dsl/start_session.rs
+++ src/middlewares/start_session/dsl/start_session.rs
@@ -51,3 +51,80 @@ pub enum StartSessionError {
     #[error("セッションの開始に失敗しました")]
     StartSessionFailed(#[source] anyhow::Error),
 }
+
+#[cfg(test)]
+mod tests {
+    use std::{convert::Infallible, future::{ready, Ready}, task::{Context, Poll}};
+
+    use http::{header::SET_COOKIE, Request, Response, StatusCode};
+    use tower::Service;
+
+    use crate::{common::{fallible::Fallible, id::account_id::AccountId, session::{refresh_pair_expiration::RefreshPairExpirationSeconds, refresh_token::RefreshToken, session_expiration::SessionExpirationSeconds, session_id::SessionId, session_series::SessionSeries}}, middlewares::start_session::dsl::{assign_refresh_pair::{AssignRefreshPair, AssignRefreshPairError}, assign_session_id::{AssignSessionId, AssignSessionIdError}}};
+
+    use super::StartSession;
+
+    struct MockStartSession;
+
+    impl StartSession for MockStartSession {}
+
+    impl AssignSessionId for MockStartSession {
+        async fn try_assign_new_session_id_with_expiration_if_unused(&self, _: &SessionId, _: AccountId, _: SessionExpirationSeconds) -> Fallible<(), AssignSessionIdError> {
+            Ok(())
+        }
+    }
+
+    impl AssignRefreshPair for MockStartSession {
+        async fn try_assign_refresh_pair_with_expiration_if_unused(&self, _: &SessionSeries, _: &RefreshToken, _: AccountId, _: RefreshPairExpirationSeconds) -> Fallible<(), AssignRefreshPairError> {
+            Ok(())
+        }
+    }
+
+    struct MockService;
+
+    impl Service<Request<()>> for MockService {
+        type Response = Response<()>;
+        type Error = Infallible;
+        type Future = Ready<Result<Self::Response, Self::Error>>;
+
+        fn poll_ready(&mut self, _: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+            Poll::Ready(Ok(()))
+        }
+
+        // 下位ハンドラを模倣し、成功時のみアカウントIDをextensionにセットする
+        fn call(&mut self, request: Request<()>) -> Self::Future {
+            if let Some(account_id) = request.extensions().get::<AccountId>() {
+                let mut response = Response::new(());
+                response.extensions_mut().insert(account_id.clone());
+                ready(Ok(response))
+            } else {
+                let response = Response::builder()
+                    .status(StatusCode::INTERNAL_SERVER_ERROR)
+                    .body(())
+                    .unwrap();
+                ready(Ok(response))
+            }
+        }
+    }
+
+    async fn test_start_session(account_id: Option<AccountId>, expected_set_cookie_headers: usize) {
+        let mut request = Request::new(());
+        if let Some(account_id) = account_id {
+            request.extensions_mut().insert(account_id);
+        }
+
+        let response = MockStartSession.start_session(&mut MockService, request).await.unwrap();
+        let set_cookie_headers = response.headers().get_all(SET_COOKIE).iter().count();
+
+        assert_eq!(set_cookie_headers, expected_set_cookie_headers);
+    }
+
+    #[tokio::test]
+    async fn start_session_success() {
+        test_start_session(Some(AccountId::gen()), 2).await;
+    }
+
+    #[tokio::test]
+    async fn start_session_failure() {
+        test_start_session(None, 0).await;
+    }
+}
