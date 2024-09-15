03836895aaa54ee2c0e0361c9af73ea2b94f1a57
amata1219 <suzukiamata@gmail.com>
2024-09-15 15:13:42 +0900

    追加: セッションID割り当てを表現するDSL

--- src/middlewares/start_session/dsl/assign_session_id.rs
+++ src/middlewares/start_session/dsl/assign_session_id.rs
@@ -1,3 +1,28 @@
+use thiserror::Error;
+
+use crate::common::{fallible::Fallible, id::AccountId, session::{session_expiration::SessionExpirationSeconds, session_id::SessionId}};
+
 pub(crate)  trait AssignSessionId {
-    
+    async fn assign_session_id(&self, session_account_id: &AccountId, new_expiration: &SessionExpirationSeconds) -> Fallible<SessionId, AssignSessionIdError> {
+        let mut new_session_id = SessionId::gen();
+        
+        // このループは奇跡が起きない限りO(1)となる
+        loop {
+            match self.try_assign_new_session_id_with_expiration_if_unused(&new_session_id, session_account_id, new_expiration).await {
+                Ok(()) => return Ok(new_session_id),
+                Err(AssignSessionIdError::SessionIdAlreadyUsed) => new_session_id = SessionId::gen(),
+                Err(e) => return Err(e),
+            }
+        }
+    }
+
+    async fn try_assign_new_session_id_with_expiration_if_unused(&self, new_session_id: &SessionId, session_account_id: &AccountId, new_expiration: &SessionExpirationSeconds) -> Fallible<(), AssignSessionIdError>;
+}
+
+#[derive(Debug, Error)]
+pub enum AssignSessionIdError {
+    #[error("セッションIDが既に使用されています")]
+    SessionIdAlreadyUsed,
+    #[error("新規セッションIDの割り当てに失敗しました")]
+    AssignNewSessionIdFailed(#[source] anyhow::Error),
 }
