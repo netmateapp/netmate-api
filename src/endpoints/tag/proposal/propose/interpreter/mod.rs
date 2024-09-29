91f25782cd95575c06806f41a68deaa0a7ef4ba7
amata1219 <suzukiamata@gmail.com>
2024-09-29 16:51:08 +0900

    整理: テーブル名の変更

--- src/endpoints/tag/proposal/propose/interpreter/mod.rs
+++ src/endpoints/tag/proposal/propose/interpreter/mod.rs
@@ -12,20 +12,20 @@ pub struct ProposeTagRelationImpl {
     cache: Arc<Pool>,
     select_tag_relation: Arc<PreparedStatement>,
     select_top_tag: Arc<PreparedStatement>,
-    insert_tag_relation: Arc<PreparedStatement>,
+    insert_tag_relation_proposal: Arc<PreparedStatement>,
     insert_tag_relation_rating: Arc<PreparedStatement>,
 }
 
 impl ProposeTagRelationImpl {
     pub async fn try_new(db: Arc<Session>, cache: Arc<Pool>) -> Result<Self, InitError<Self>> {
-        let select_tag_relation = prepare(&db, "SELECT inclusion_or_equivalence FROM proposed_tag_relations WHERE subtag_id = ? AND supertag_id = ?").await?;
+        let select_tag_relation = prepare(&db, "SELECT inclusion_or_equivalence FROM tag_relation_proposals WHERE subtag_id = ? AND supertag_id = ?").await?;
 
-        let select_top_tag = prepare(&db, "SELECT supertag_id FROM proposed_tag_relations WHERE subtag_id = ? LIMIT 1").await?;
+        let select_top_tag = prepare(&db, "SELECT supertag_id FROM tag_relation_proposals WHERE subtag_id = ? LIMIT 1").await?;
 
-        let insert_tag_relation = prepare(&db, "INSERT INTO proposed_tag_relations (subtag_id, supertag_id, inclusion_or_equivalence, language_group) VALUES (?, ?, ?, ?) IF NOT EXISTS").await?;
+        let insert_tag_relation_proposal = prepare(&db, "INSERT INTO tag_relation_proposals (subtag_id, supertag_id, inclusion_or_equivalence, language_group, proposer_id. proposed_at) VALUES (?, ?, ?, ?, ?, ?) IF NOT EXISTS").await?;
 
         let insert_tag_relation_rating = prepare(&db, "INSERT INTO tag_relation_ratings_by_account (account_id, subtag_id, supertag_id, inclusion_or_equivalence, operation_id) VALUES (?, ?, ?, ?, 127)").await?;
 
-        Ok(Self { db, cache, select_tag_relation, select_top_tag, insert_tag_relation, insert_tag_relation_rating })
+        Ok(Self { db, cache, select_tag_relation, select_top_tag, insert_tag_relation_proposal, insert_tag_relation_rating })
     }
 }
