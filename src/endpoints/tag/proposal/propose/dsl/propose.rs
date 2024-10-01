44ede1d30f5c939aa291714ac7f36840f0d7eded
amata1219 <suzukiamata@gmail.com>
2024-10-01 23:13:41 +0900

    追加: 自動テスト

--- src/endpoints/tag/proposal/propose/dsl/propose.rs
+++ src/endpoints/tag/proposal/propose/dsl/propose.rs
@@ -19,6 +19,7 @@ pub(crate) trait ProposeTagRelation {
                 if self.has_already_been_proposed(subtag_id, supertag_id, relation).await? {
                     Err(ProposeTagRelationError::HasAlreadyBeenProposed)
                 } else {
+                    // 存在しないタグはここで弾かれる
                     let (subtag_language_group, subtag_name) = self.get_language_group_and_tag_name(subtag_id).await?;
                     let (supertag_language_group, supertag_name) = self.get_language_group_and_tag_name(supertag_id).await?;
     
@@ -71,4 +72,85 @@ pub enum ProposeTagRelationError {
     UpdateTagRelationListFailed(#[source] RelateHierarchicalTagsError),
     #[error("タグ関係の提案に失敗しました")]
     ProposeTagRelationFailed(#[source] anyhow::Error),
+}
+
+#[cfg(test)]
+mod tests {
+    use std::{str::FromStr, sync::LazyLock};
+
+    use crate::{common::{fallible::Fallible, profile::account_id::AccountId, tag::{language_group::LanguageGroup, non_top_tag::NonTopTagId, relation::TagRelation, tag_id::TagId, tag_name::TagName}, uuid::uuid4::Uuid4}, endpoints::tag::proposal::propose::dsl::{relate_hierarchical_tags::{RelateHierarchicalTags, RelateHierarchicalTagsError}, validate_topology::{ValidateTopology, ValidateTopologyError}}, helper::test::mock_uuid};
+
+    use super::{ProposeTagRelation, ProposeTagRelationError};
+
+    struct MockProposeTagRelation;
+
+    impl ValidateTopology for MockProposeTagRelation {
+        async fn is_acyclic(&self, _: NonTopTagId, _: NonTopTagId) -> Fallible<bool, ValidateTopologyError> {
+            Ok(true)
+        }
+
+        async fn is_equivalent(&self, _: NonTopTagId, _: NonTopTagId) -> Fallible<bool, ValidateTopologyError> {
+            Ok(true)
+        }
+    }
+
+    impl RelateHierarchicalTags for MockProposeTagRelation {
+        async fn relate_by_inclusion(&self, _: NonTopTagId, _: TagName, _: NonTopTagId, _: TagName) -> Fallible<(), RelateHierarchicalTagsError> {
+            Ok(())
+        }
+
+        async fn relate_by_equivalence(&self, _: NonTopTagId, _: TagName, _: NonTopTagId, _: TagName) -> Fallible<(), RelateHierarchicalTagsError> {
+            Ok(())
+        }
+    }
+
+    static PROPOSED: LazyLock<NonTopTagId> = LazyLock::new(|| NonTopTagId::try_from(TagId::of(Uuid4::new_unchecked(mock_uuid(1)))).unwrap());
+    static EXISTENT1: LazyLock<NonTopTagId> = LazyLock::new(|| NonTopTagId::try_from(TagId::of(Uuid4::new_unchecked(mock_uuid(2)))).unwrap());
+    static EXISTENT2: LazyLock<NonTopTagId> = LazyLock::new(|| NonTopTagId::try_from(TagId::of(Uuid4::new_unchecked(mock_uuid(3)))).unwrap());
+
+    impl ProposeTagRelation for MockProposeTagRelation {
+        async fn has_already_been_proposed(&self, subtag_id: NonTopTagId, _: NonTopTagId, _: TagRelation) -> Fallible<bool, ProposeTagRelationError> {
+            if subtag_id == *PROPOSED {
+                Ok(true)
+            } else {
+                Ok(false)
+            }
+        }
+
+        async fn fetch_language_group_and_tag_name(&self, tag_id: NonTopTagId) -> Fallible<Option<(LanguageGroup, TagName)>, ProposeTagRelationError> {
+            if tag_id == *EXISTENT1 || tag_id == *EXISTENT2 {
+                Ok(Some((LanguageGroup::Japanese, TagName::from_str("test").unwrap())))
+            } else {
+                Ok(None)
+            }
+        }
+    
+        async fn propose(&self, _: AccountId, _: NonTopTagId, _: NonTopTagId, _: TagRelation, _: LanguageGroup) -> Fallible<(), ProposeTagRelationError> {
+            Ok(())
+        }
+    }
+
+    async fn test_dsl(subtag_id: NonTopTagId, supertag_id: NonTopTagId) -> Result<(), ProposeTagRelationError> {
+        MockProposeTagRelation.propose_tag_relation(AccountId::gen(), subtag_id, supertag_id, TagRelation::Inclusion).await
+    }
+
+    #[tokio::test]
+    async fn proposed() {
+        assert!(matches!(test_dsl(*PROPOSED, NonTopTagId::gen()).await.err().unwrap(), ProposeTagRelationError::HasAlreadyBeenProposed));
+    }
+
+    #[tokio::test]
+    async fn partial_existent() {
+        assert!(matches!(test_dsl(*EXISTENT1, NonTopTagId::gen()).await.err().unwrap(), ProposeTagRelationError::NonExistentTag));
+    }
+    
+    #[tokio::test]
+    async fn existent() {
+        assert!(test_dsl(*EXISTENT1, *EXISTENT2).await.is_ok());
+    }
+
+    #[tokio::test]
+    async fn non_existent() {
+        assert!(matches!(test_dsl(NonTopTagId::gen(), NonTopTagId::gen()).await.err().unwrap(), ProposeTagRelationError::NonExistentTag));
+    }
 }
