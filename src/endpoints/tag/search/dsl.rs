ba71ba92227dd0d220ec9242aa6151bfcf8939c6
amata1219 <suzukiamata@gmail.com>
2024-10-03 00:43:45 +0900

    追加: タグ検索インタプリタ

--- src/endpoints/tag/search/dsl.rs
+++ src/endpoints/tag/search/dsl.rs
@@ -1,8 +1,9 @@
 use std::collections::HashMap;
 
+use serde::{ser::SerializeStruct, Serialize};
 use thiserror::Error;
 
-use crate::common::{fallible::Fallible, tag::{hierarchy::TagHierarchy, language_group::LanguageGroup, tag_id::TagId, tag_info::TagInfo, tag_name::TagName}};
+use crate::common::{consensus::{proposal::IsProposal, stability::Stability}, fallible::Fallible, tag::{hierarchy::TagHierarchy, language_group::LanguageGroup, tag_id::TagId, tag_name::TagName}};
 
 // search_afterを使う
 pub(crate) trait SearchWithinHierarchicalTagList {
@@ -22,6 +23,10 @@ pub(crate) trait SearchWithinHierarchicalTagList {
         
         // マッチしたタグのIDを抽出
         let matched_tag_ids: Vec<TagId> = matched_tags.iter().map(|(tag_id, _)| *tag_id).collect();
+
+        if matched_tag_ids.is_empty() {
+            return Ok(Vec::new());
+        }
         
         // タグ情報をフェッチ
         let tag_info_map = self
@@ -32,12 +37,10 @@ pub(crate) trait SearchWithinHierarchicalTagList {
         let tag_infos = matched_tags
             .into_iter()
             .map(|(tag_id, tag_name)| {
-                // タグ情報が存在しない場合は (false, false) を使用
-                let (is_proposal, is_stable) = tag_info_map.get(&tag_id)
-                    .copied()
-                    .unwrap_or((false, false));
-                
-                TagInfo::new(tag_id, tag_name, is_proposal, is_stable)
+                match tag_info_map.get(&tag_id) {
+                    Some((is_proposal, stability)) => TagInfo::new(tag_id, tag_name, true, *is_proposal, *stability),
+                    None => TagInfo::new(tag_id, tag_name, false, IsProposal::NotProposal, Stability::Unstable)
+                }
             })
             .collect::<Vec<TagInfo>>();
         
@@ -51,14 +54,14 @@ pub(crate) trait SearchWithinHierarchicalTagList {
         search_after: Option<TagId>
     ) -> Fallible<Vec<(TagId, TagName)>, SearchWithinHierarchicalTagListError>;
 
-    // (bool, bool) は is_proposal, is_stable
+    // tagsは空ではないことが保証されている
     async fn fetch_tag_info(
         &self,
         tag_id: TagId,
         related_tag_id: TagId,
         hierarchy: TagHierarchy,
         tags: Vec<TagId>
-    ) -> Fallible<HashMap<TagId, (bool, bool)>, SearchWithinHierarchicalTagListError>;
+    ) -> Fallible<HashMap<TagId, (IsProposal, Stability)>, SearchWithinHierarchicalTagListError>;
 }
 
 #[derive(Debug, Error)]
@@ -69,4 +72,50 @@ pub enum SearchWithinHierarchicalTagListError {
     FetchTagInfoFailed(#[source] anyhow::Error),
     #[error("階層別タグ一覧内の検索に失敗しました")]
     SearchWithinHierarchicalTagListFailed(#[source] anyhow::Error),
+}
+
+pub struct TagInfo {
+    id: TagId,
+    name: TagName,
+    is_reachable: bool,
+    is_proposal: IsProposal,
+    is_stable: Stability,
+}
+
+impl TagInfo {
+    pub fn new(id: TagId, name: TagName, is_reachable: bool, is_proposal: IsProposal, is_stable: Stability) -> Self {
+        TagInfo { id, name, is_reachable, is_proposal, is_stable }
+    }
+
+    pub fn id(&self) -> &TagId {
+        &self.id
+    }
+
+    pub fn name(&self) -> &TagName {
+        &self.name
+    }
+
+    pub fn is_reachable(&self) -> bool {
+        self.is_reachable
+    }
+
+    pub fn is_proposal(&self) -> IsProposal {
+        self.is_proposal
+    }
+
+    pub fn is_stable(&self) -> Stability {
+        self.is_stable
+    }
+}
+
+impl Serialize for TagInfo {
+    fn serialize<S: serde::Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
+        let mut state = serializer.serialize_struct("TagInfo", 5)?;
+        state.serialize_field("id", &self.id)?;
+        state.serialize_field("name", &self.name)?;
+        state.serialize_field("is_reachable", &self.is_reachable)?;
+        state.serialize_field("is_proposal", &bool::from(self.is_proposal()))?;
+        state.serialize_field("is_stable", &bool::from(self.is_stable()))?;
+        state.end()
+    }
 }
--- src/endpoints/tag/search/interpreter.rs
+++ src/endpoints/tag/search/interpreter.rs
@@ -5,7 +5,7 @@ use scylla::{prepared_statement::PreparedStatement, Session};
 use serde_json::{json, Value};
 use uuid::Uuid;
 
-use crate::{common::{fallible::Fallible, tag::{hierarchy::TagHierarchy, language_group::LanguageGroup, tag_id::TagId, tag_info::TagInfo, tag_name::TagName}, uuid::uuid4::Uuid4}, helper::{error::InitError, scylla::prepare}};
+use crate::{common::{consensus::{proposal::IsProposal, stability::Stability}, fallible::Fallible, tag::{hierarchy::TagHierarchy, language_group::LanguageGroup, tag_id::TagId, tag_name::TagName}, uuid::uuid4::Uuid4}, helper::{error::InitError, scylla::prepare}};
 
 use super::dsl::{SearchWithinHierarchicalTagList, SearchWithinHierarchicalTagListError};
 
@@ -17,11 +17,13 @@ pub struct SearchWithinHierarchicalTagListImpl {
 
 impl SearchWithinHierarchicalTagListImpl {
     pub async fn try_new(&self, db: Arc<Session>, client: Arc<Elasticsearch>) -> Result<Self, InitError<Self>> {
-        let select_tags_info = prepare(&db, "SELECT related_tag_id, is_unstable_proposal, is_status_calculated FROM hierarchical_tag_lists WHERE tag_id = ? AND relation = ? AND related_tag_id IN (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)").await?;
+        let select_tags_info = prepare(&db, "SELECT related_tag_id, is_proposal, is_stable FROM hierarchical_tag_lists WHERE tag_id = ? AND hierarchy = ? AND related_tag_id IN (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)").await?;
 
         Ok(Self { db, client, select_tags_info })
     }
 }
+
+const PAGE_SIZE: usize = 10;
 
 impl SearchWithinHierarchicalTagList for SearchWithinHierarchicalTagListImpl {
     async fn search_matched_tags(&self, query: TagName, language_group: LanguageGroup, search_after: Option<TagId>) -> Fallible<Vec<(TagId, TagName)>, SearchWithinHierarchicalTagListError> {
@@ -48,7 +50,7 @@ impl SearchWithinHierarchicalTagList for SearchWithinHierarchicalTagListImpl {
               { "tag_id": "asc" }
             ],
             "_source": ["id", "name"],
-            "size": 10
+            "size": PAGE_SIZE
         });
 
         // search_afterがある場合は、それをクエリに追加
@@ -86,17 +88,54 @@ impl SearchWithinHierarchicalTagList for SearchWithinHierarchicalTagListImpl {
         Ok(matched_tags)
     }
 
-    async fn fetch_tag_info(&self, tag_id: TagId, related_tag_id: TagId, hierarchy: TagHierarchy, tags: Vec<TagId>) -> Fallible<HashMap<TagId, (bool, bool)>, SearchWithinHierarchicalTagListError> {
-        if tags.len() < 10 {
-            // tagsの要素が10個未満の場合は、最後の要素で埋める
-            let last_tag_id = tags.last().unwrap();
-            let diff = 10 - tags.len();
-            let mut tags = tags.clone();
-            for _ in 0..diff {
-                tags.push(*last_tag_id);
-            }
+    async fn fetch_tag_info(&self, tag_id: TagId, related_tag_id: TagId, hierarchy: TagHierarchy, mut tags: Vec<TagId>) -> Fallible<HashMap<TagId, (IsProposal, Stability)>, SearchWithinHierarchicalTagListError> {
+        extend_to_length(&mut tags);
+
+        fn get(tags: &[TagId], i: usize) -> TagId {
+          *tags.get(i).unwrap()
         }
 
-        Ok(HashMap::new())
+        let tag_infos: HashMap<TagId, (IsProposal, Stability)> = self.db
+            .execute_unpaged(&self.select_tags_info, (tag_id, hierarchy, related_tag_id, get(&tags, 0), get(&tags, 1), get(&tags, 2), get(&tags, 3), get(&tags, 4), get(&tags, 5), get(&tags, 6), get(&tags, 7), get(&tags, 8), get(&tags, 9)))
+            .await
+            .map_err(|e| SearchWithinHierarchicalTagListError::FetchTagInfoFailed(e.into()))?
+            .rows_typed()
+            .map_err(|e| SearchWithinHierarchicalTagListError::FetchTagInfoFailed(e.into()))?
+            .flatten()
+            .map(|(tag_id, is_proposal, stability)| (tag_id, (is_proposal, stability)))
+            .collect();
+
+        Ok(tag_infos)
+    }
+}
+
+// tagsが空ではない前提
+fn extend_to_length(tags: &mut Vec<TagId>) {
+  if tags.len() < PAGE_SIZE {
+    // tagsの要素が10個未満の場合は、最後の要素で埋める
+    let last_tag_id = *tags.last().unwrap();
+    let diff = PAGE_SIZE - tags.len();
+    for _ in 0..diff {
+        tags.push(last_tag_id);
+    }
+  }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{common::tag::tag_id::TagId, endpoints::tag::search::interpreter::extend_to_length};
+
+    use super::PAGE_SIZE;
+
+    #[test]
+    fn test_extend_to_length() {
+        for i in 0..PAGE_SIZE {
+            let mut vec = vec![];
+            for _ in 0..=i {
+                vec.push(TagId::gen());
+            }
+            extend_to_length(&mut vec);
+            assert_eq!(vec.len(), 10);
+        }
     }
 }
